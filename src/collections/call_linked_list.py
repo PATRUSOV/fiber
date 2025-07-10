7904328ffaa8aebf15a2a0daf09c70b8a60ecc23
kotmarkot <andrey.patrusov@gmail.com>
2025-07-10 02:31:58 +0000

    Рефакторин: +Мелкая правка доков +CallNode() теперь Generic

--- src/collections/call_linked_list.py
+++ src/collections/call_linked_list.py
@@ -1,24 +1,27 @@
 from dataclasses import dataclass
-from typing import Optional, Type, Union, List, Tuple
+from typing import Optional, Type, Union, List, Tuple, Generic
 
 from src.step import Step
+from src.step.types import I, O
 
 
 @dataclass
-class CallNode:
+class CallNode(Generic[I, O]):
     """
     Класс вызова, содержит данные текущего вызова то есть статический класс реализующий Step.
     Дополнительно хранит ссылку на следующий вызов. Если верщина являетьяс последней, то next = None.
 
     Attributes:
-        cls: Статический класс реализующий Step.
+        step: Статический класс реализующий Step.
         next: Следующий вызов.
     """
 
-    step: Type[Step]
+    step: Type[Step[I, O]]
     next: Optional["CallNode"]
 
 
+# TODO:
+#   +Добавить проверкук типов первого и последнего типа.
 def get_call_head(steps: Union[List[Type[Step]], Tuple[Type[Step], ...]]) -> CallNode:
     """
     Преобразует [Список, Кортеж] в односвязный список.
