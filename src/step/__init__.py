92214a14d4b800535ac2ee7cbdf9ad4d31a9c6a7
kotmarkot <andrey.patrusov@gmail.com>
2025-07-10 02:23:42 +0000

    Добавлен функция для извлечения типов из Step(Generic). Типы Generic вынесены в моудль с типами.

--- src/step/__init__.py
+++ src/step/__init__.py
@@ -1,4 +1,5 @@
 from src.step.core import Step
+from src.step.types import get_step_types
 
 
-__all__ = ["Step"]
+__all__ = ["Step", "get_step_types"]
---
+++ src/step/types.py
@@ -0,0 +1,33 @@
+from logging import Logger
+from typing import Type, Tuple, Any, TypeVar, Final, get_origin, get_args
+from src.step.core import Step
+
+from src.logging_manager import LoggingManager
+
+I: Final = TypeVar("I")
+O: Final = TypeVar("O")
+
+
+def get_step_types(
+    step: Type[Step[I, O]], logger: Logger = LoggingManager.get_kernel_logger()
+) -> Tuple[Type[Any], Type[Any]]:
+    """
+    Возвращает входной и выходной тип шага.
+
+    Arguments:
+        step: Шаг.
+        logger: Логер используемый функцией, по умолчанию - логгер ядра фрейморка.
+
+    Returns:
+        Кортеж из типа входных данных и типа выходных данных.
+    """
+    if issubclass(step, Step):
+        bases = getattr(step, "__orig_bases__", [])
+        for base_cls in bases:
+            if get_origin(base_cls) is Step:
+                input_type, output_type = get_args(base_cls)
+                return input_type, output_type
+
+    error_mes = f"Шаг {step.__name__} не являеться наследником Step"
+    logger.fatal(error_mes)
+    raise TypeError(error_mes)
