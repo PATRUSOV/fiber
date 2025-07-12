7811c8f798e756dd437e63329ede4ed679054a66
kotmarkot <andrey.patrusov@gmail.com>
2025-07-12 01:44:57 +0000

    Добавлена impr_isinstance()

---
+++ src/utils/types.py
@@ -0,0 +1,25 @@
+from logging import Logger
+from typing import Any, Type
+
+
+def impr_isinstance(
+    data: Any, expected_type: Type[Any], error_msg: str, logger: Logger
+) -> None:
+    """
+    Выполняет проверку типа на принадлежность типу.
+    Особенности:
+        +data = None, expected_type = None => True
+        +data = ..., expected_type = Any => True
+
+    Args:
+        data (Any): Проверяемое значение.
+        expected_type (Type[Any]): Ожидаемый тип, извлечённый из Step.
+        error_msg (str): сообщение выводимое при ошибке.
+        logger (Logger): Обект логгера для соообщения об ошибке.
+
+    Raises:
+        TypeError: если тип `data` не соответствует ожидаемому.
+    """
+    if expected_type is not Any and not isinstance(data, (type(None), expected_type)):
+        logger.fatal(error_msg)
+        raise TypeError(error_msg)
