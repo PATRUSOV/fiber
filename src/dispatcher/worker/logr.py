7ceac45b293ee9c2069345d237c4a105a33dc111
kotmarkot <andrey.patrusov@gmail.com>
2025-07-19 20:23:06 +0000

    Добавлено: +Документация к get_worker_logger()

--- src/dispatcher/worker/logr.py
+++ src/dispatcher/worker/logr.py
@@ -6,6 +6,13 @@ worker_counter = 0
 
 
 def get_worker_logger() -> Logger:
+    """
+    Функция выдвает логгер для Worker(), ведет учет количества
+    логгер и при создании увиличивает глобальную переменную worker_counter.
+
+    Returns:
+        Logger унаследованный от логера ядра.
+    """
     global worker_counter
     kernel_logger = lm.get_kernel_logger()
     worker_logger = kernel_logger.getChild(f"worker-{worker_counter}")
