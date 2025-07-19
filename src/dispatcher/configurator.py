0f8cd3ae6e494caacf2c75dc421503ae80d0e885
kotmarkot <andrey.patrusov@gmail.com>
2025-07-19 20:14:51 +0000

    Добавлено: +Минимальная документая для DispatcherConfig

--- src/dispatcher/configurator.py
+++ src/dispatcher/configurator.py
@@ -3,6 +3,15 @@ from dataclasses import dataclass
 
 @dataclass
 class DispatcherConfig:
+    """
+    Конфигуратор для Dispatcher.
+
+    Attrs:
+        TASK_LIMIT: Максимально допустимое количество активных задач.
+        WORKERS: Количество обработчиков (потоков). (Если в цепочке не надо ничего параллелить, то рекомендуется поставить 1.
+        TASKS_PER_ITER: Максимум генерируемых Worker-ом задач, за одно обращение к очереди.
+    """
+
     TASK_LIMIT: int
     WORKERS: int
     TASKS_PER_ITER: int
