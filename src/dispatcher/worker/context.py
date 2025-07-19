d04fb3aa358bf8a00853913bec2c5a53f7bb87f5
kotmarkot <andrey.patrusov@gmail.com>
2025-07-19 20:46:48 +0000

    Добавлено: +Документация к WorkerContext

--- src/dispatcher/worker/context.py
+++ src/dispatcher/worker/context.py
@@ -4,18 +4,33 @@ from src.dispatcher.task import Task
 
 
 class WorkerContext:
+    """
+    Контекст для Worker. Расчитывает лимит генерации и хранит данные необходимые Worker.
+    """
+
     def __init__(
         self,
         deque: ThreadSafeDeque[Task | None],
         deque_limit: int,
         max_tasks_per_iter: int,
     ):
+        """
+        Создает контекст для исполнения Worker.
+
+        Args:
+            deque: Двустороння очередь с которой будет работать Worker.
+            deque_limit: Предел к которому стремиться размер очереди (при приближении к нему генерация замедляется).
+            max_task_per_iter: Максимальное колиство задач которые воркер может сгенерировать за одно обращение к очереди.
+        """
         self.deque = deque
         self.logger = get_worker_logger()
         self._deque_limit = deque_limit
         self._max_tasks_per_iter = max_tasks_per_iter
 
     def get_generation_limit(self) -> int:
+        """
+        Вычисляет количесво задач которые воркер сможет сгенерировать за одно обращение к очереди.
+        """
         queue_size = len(self.deque)
         deque_lim = self._deque_limit
         max_task_per_iter = self._max_tasks_per_iter
