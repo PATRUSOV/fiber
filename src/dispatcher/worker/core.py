f22e881616b6df80c867df7f3a7f0c04bc2bcd4e
kotmarkot <andrey.patrusov@gmail.com>
2025-07-22 17:46:03 +0000

    Изменения: +Добавлено логгирование.

--- src/dispatcher/worker/core.py
+++ src/dispatcher/worker/core.py
@@ -15,27 +15,36 @@ class Worker:
             context: Контекст в котором будет работать воркер (см. подробнее в доках к WorkerContext).
         """
         self._context = context
+        self._context.logger.debug("Создан.")
 
     def run(self) -> None:
         """
-        Запускает воркера (придназнаено для запуска в Thread()).
+        Запускает воркера (придназнаено для запуска в Thread(target=)).
         """
+        self._context.logger.debug("Запущен.")
         while True:
             item = self._context.deque.get()
             if item is None:
+                self._context.logger.debug("Остановлен.")
                 break
             task = item
 
             generation_lim = self._context.get_generation_limit()
+            self._context.logger.debug(
+                f"Начал выполнение Task. Лимит генерации: {generation_lim}"
+            )
             for _ in range(generation_lim):
                 try:
                     next_task = task.step()
                 except TaskDone:
+                    self._context.logger.debug("Завершил выполнение Task.")
                     break
 
                 self._context.deque.put(next_task)
+                self._context.logger.debug("Добавил в очередь новый Task.")
 
             if not task.is_done():
                 self._context.deque.lput(task)
+                self._context.logger.debug("Вернул Task в очередь.")
 
             self._context.deque.task_done()
