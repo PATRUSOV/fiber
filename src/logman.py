fd74bc93d75316854bc8394bcbb0d28288213d05
kotmarkot <andrey.patrusov@gmail.com>
2025-07-22 17:28:33 +0000

    Изменения: +Добавлен документация в logman.

--- src/logman.py
+++ src/logman.py
@@ -8,13 +8,25 @@ from src import __appname__
 
 
 def get_main_logger() -> logging.Logger:
+    """
+    Возвращает:
+        Логгер для всего приложения.
+    """
     return logging.getLogger(__appname__)
 
 
 def get_main_step_logger() -> logging.Logger:
+    """
+    Возвращает:
+        Базовый логгер для от которого наследуются логгеры для каждого Step.
+    """
     return get_main_logger().getChild("step")
 
 
 def get_kernel_logger() -> logging.Logger:
+    """
+    Возвращает:
+        Логгер используемый "под капотом" у фремворка.
+    """
     main_logger = get_main_logger()
     return main_logger.getChild("kernel")
