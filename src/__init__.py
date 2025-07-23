6221389350dbd31bb073c46ef91897640b0fefc0
kotmarkot <andrey.patrusov@gmail.com>
2025-07-23 17:58:22 +0000

    Изменения: +Добавлены базовые настройки логгера.

--- src/__init__.py
+++ src/__init__.py
@@ -3,3 +3,13 @@ __author__ = "kotmarkot"
 __appname__ = "Fiber"
 
 __all__ = []
+
+import src.logman as lm
+
+from logging import Formatter, StreamHandler
+
+stream_hdlr = StreamHandler()
+stream_hdlr.setFormatter(
+    Formatter("%(asctime)s - %(levelname)s - %(name)s - %(lineno)d - %(message)s")
+)
+lm.get_main_logger().addHandler(stream_hdlr)
