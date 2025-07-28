1678c04b5dfcf4bcb0bdef39a6843ffb824ac503
kotmarkot <andrey.patrusov@gmail.com>
2025-07-28 12:21:11 +0000

    Изменения: +Добавлены исключения.

--- src/step/__init__.py
+++ src/step/__init__.py
@@ -1,5 +1,18 @@
 from src.step.core import Step
 from src.step.types import get_step_types
+from src.step.exceptions import (
+    NotAStepError,
+    StepTypeExtractionError,
+    StepTypeParameterCountMismatch,
+    StepTypeParametersMissing,
+)
 
 
-__all__ = ["Step", "get_step_types"]
+__all__ = [
+    "Step",
+    "get_step_types",
+    "NotAStepError",
+    "StepTypeExtractionError",
+    "StepTypeParameterCountMismatch",
+    "StepTypeParametersMissing",
+]
