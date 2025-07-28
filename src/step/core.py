4f190290ce0d01569c8c31c99416a565cd53b24f
kotmarkot <andrey.patrusov@gmail.com>
2025-07-28 13:39:50 +0000

    Изменения: +Проверка Generic вынесена в инициализацию наследника. +Правлены доки.

--- src/step/core.py
+++ src/step/core.py
@@ -3,7 +3,12 @@ import src.logman as lm
 from src.step.vars import I, O
 from abc import abstractmethod, ABC
 from logging import Logger
-from typing import Generic, Generator, Union
+from typing import Generic, Generator, Union, get_args, get_origin
+from step.exceptions import (
+    StepTypeParameterCountMismatch,
+    StepTypeParametersMissing,
+    StepTypeExtractionError,
+)
 
 
 class Step(ABC, Generic[I, O]):
@@ -18,13 +23,25 @@ class Step(ABC, Generic[I, O]):
     к которым могут попытаться обратиться сразу несколько шагов из нескольких потоков одновременно (например:
     файлы, базы данных и т.д.), полностью лежит на mutex-ах, очередях и прочих ThreadSafe механизмах, реализованных внутри шагов.
 
-    Attributes:
+    - Особенности: вся валидация происходит при наследование -> класс наследника гарантировано будет корректным.
+
+    Аттрибуты:
         start(): абстрактный статический метод, вызываемый у наследника при выполнении цепочки.
         logger (Logger): Логгер выделяемый шагу. Рекомнедуется при разработки ипользовать именно его
         и его наследников для централизованого и безопасного логгирования.
 
-    Example:
+    Исключения:
+        StepTypeParametersMissing:
+            Параметры типов не указаны (Step без [I, O]).
 
+        StepTypeParameterCountMismatch:
+            Количесво параметров типов не равно двуx (например, Step[int], Step[int, str, float]).
+
+        StepTypeExtractionError:
+            Step[...] не найден в __orig_bases__, вероятно, скрыт в глубокой иерархии.
+
+
+    Примеры:
         >>> class Something: ...
         >>> class Anything: ...
 
@@ -53,7 +70,7 @@ class Step(ABC, Generic[I, O]):
     @abstractmethod
     def start(cls, data: I) -> Union[O, Generator[O, None, None]]:
         """
-        Абстрактный статический метод. Является основной точкой входа для каждого шага.
+        Абстрактный классовый метод. Является основной точкой входа для каждого шага.
         В случае если шаг первый первый, то он не должен принимать None в качестве I, а если последний
         то O.
 
@@ -84,6 +101,43 @@ class Step(ABC, Generic[I, O]):
         """
 
     def __init_subclass__(cls, **kwargs) -> None:
-        """Выдает шагу логгер"""
         super().__init_subclass__(**kwargs)
+
+        cls._init_logger()
+        cls._validate_generic_params()
+
+    @classmethod
+    def _init_logger(cls) -> None:
+        """Передает наследнику логгер."""
         cls.logger = lm.get_main_step_logger().getChild(cls.__name__)
+
+    @classmethod
+    def _validate_generic_params(cls) -> None:
+        """Проверяет корректно ли наследник унаследовал Step."""
+
+        # for-else - питоновская кострукция работающая по приниципу:
+        # break не было, и цикл завершен - вызов else
+        for base in getattr(cls, "__orig_bases__", []):
+            # получение обекта класса
+            if get_origin(base) is Step:
+                # получение аргументов Generic-а
+                args = get_args(base)
+
+                # проверка на отсутствие аргументов
+                if len(args) == 0:
+                    error_msg = f"{cls.__name__} унаследован от Step, но параметры типа не указаны."
+                    cls.logger.fatal(error_msg)
+                    raise StepTypeParametersMissing(error_msg)
+
+                # проверка на недостаточное или избыточное количество аргументов
+                if len(args) != 2:
+                    error_msg = f"{cls.__name__} должен наследовать Step с двумя параметрами типа, но получено {len(args)}: {args}"
+                    cls.logger.fatal(error_msg)
+                    raise StepTypeParameterCountMismatch(error_msg)
+
+                break
+        else:
+            # если Step[I, O] вообще не нашёлся
+            error_msg = f"{cls.__name__} не содержит Step[...] в __orig_bases__."
+            cls.logger.fatal(error_msg)
+            raise StepTypeExtractionError(error_msg)
--- src/step/types.py
+++ src/step/types.py
@@ -1,12 +1,7 @@
 import src.logman as lm
 
 from src.step.core import Step
-from src.step.exceptions import (
-    NotAStepError,
-    StepTypeParameterCountMismatch,
-    StepTypeParametersMissing,
-    StepTypeExtractionError,
-)
+from src.step.exceptions import NotAStepError, StepTypeExtractionError
 from logging import Logger
 from typing import Type, Tuple, Any, get_origin, get_args
 
@@ -34,25 +29,12 @@ def get_step_types(
 
     Исключения:
         NotAStepError:
-            Брошено, если переданный класс не является наследником Step.
-
-        StepTypeParametersMissing:
-            Класс унаследован от Step, но параметров типов не указано вообще (Step без [I, O]).
-
-        StepTypeParameterCountMismatch:
-            Указано не два параметра типа (например, Step[int], Step[int, str, float]).
-
-        StepTypeExtractionError:
-            Step[...] не найден в __orig_bases__, вероятно, скрыт в глубокой иерархии.
+            Если переданный класс не является наследником Step.
 
     Пример:
         >>> class MyStep(Step[int, str]): ...
         >>> get_step_types(MyStep)
         (<class 'int'>, <class 'str'>)
-
-        >>> class InvalidStep(Step): ...
-        >>> get_step_types(InvalidStep)
-        # → StepTypeParametersMissing
 
         >>> class NotAStep: ...
         >>> get_step_types(NotAStep)
@@ -71,21 +53,8 @@ def get_step_types(
             # получение аргументов Generic-а
             args = get_args(base)
 
-            # проврка на отсутвие арументов
-            if len(args) == 0:
-                error_msg = f"{step.__name__} унаследован от Step, но параметры типа не указаны."
-                logger.fatal(error_msg)
-                raise StepTypeParametersMissing(error_msg)
-
-            # проверка на недостаток / избыток аргументов
-            if len(args) != 2:
-                error_msg = f"{step.__name__} должен наследовать Step с двумя параметрами типа, но получено {len(args)}: {args}"
-                logger.fatal(error_msg)
-                raise StepTypeParameterCountMismatch(error_msg)
-
             return args
 
-    # если Step[I, O] вообще не нашёлся
-    error_msg = f"{step.__name__} не содержит Step[...] в __orig_bases__ — возможно, Step скрыт в иерархии."
-    logger.fatal(error_msg)
+    error_msg = f"{step.__name__} не содержит Step[...] в __orig_bases__."
+    step.logger.fatal(error_msg)
     raise StepTypeExtractionError(error_msg)
